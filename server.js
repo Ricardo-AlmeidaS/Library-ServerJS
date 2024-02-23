36cd23638a58e472878f8f1a162e1abb2a3fd099
Ricardo Chiarelli <137703875+Ricardo-Chiarelli@users.noreply.github.com>
2024-02-23 18:34:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-<<<<<<< HEAD
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`server listening on http://localhost:${port}`);
-=======
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
->>>>>>> 3e921cc0ea20296b285a5910c7513c7f0ebed0af
 });
